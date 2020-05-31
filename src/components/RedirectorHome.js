d4d992e4928a1531f3414ac179132d260ccc690b
eyal karbel <eyalkerbal@gmail.com>
2020-05-31 13:59:50 +0300

    jdjdds

--- src/components/RedirectorHome.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Redirect } from 'react-router-dom'
-export default class RedirectorHome extends React.Component {
-    render() {
-        return (
-            <Redirect to="/home" />
-        );
-    }
-}
--- src/components/RedirectorLogin.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Redirect } from 'react-router-dom'
-export default class RedirectorLogin extends React.Component {
-    render() {
-        return (
-            <Redirect to="/login" />
-        );
-    }
-}
