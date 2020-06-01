f1354e3154cbee56f8388a4925d78a63b6c386c1
eyal karbel <eyalkerbal@gmail.com>
2020-06-01 12:03:48 +0300

    fkjfkddoo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import Switcher from "./Switcher";
+import "./styles/style.css";
+import Switcher from "./Switcher";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <div className="height100">
-    jdjsxzskxzjk
+    <Switcher />
   </div>,
   document.getElementById("root")
 );
 
+serviceWorker.register();
