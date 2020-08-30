9eda95516f6560c754f7873d6c8a0bf05a13b303
eyal karbel <eyalkerbal@gmail.com>
2020-08-30 12:09:05 +0300

    exchanget

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,39 @@
 import { combineReducers } from 'redux';
-import user from './UserReducer';
-import notification from './notifactionReducer';
+// import user from './UserReducer';
+// import notification from './notifactionReducer';
+
+const notification = (state = [{}], action) => {
+    switch(action.type) {
+        case "SetNotification":
+            console.log("date" , action.date)
+            return [...state,
+                {
+                 date: action.date
+                } ];
+        default:
+            return state;       
+    }
+}
+
+
+const user = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case "Login":
+       return {
+           user: action.user,
+           sn: action.sn,
+           password: action.password
+        };
+        case "LogOut":
+         return INITIAL_STATE;
+      default:
+        return state;
+    }
+
+  }
+  
+
 export default combineReducers({
     user,
     notification
-
 });
