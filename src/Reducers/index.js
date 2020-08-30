0eb632eb5bf532eb6feb6b14a95626fd2cae9c84
eyal karbel <eyalkerbal@gmail.com>
2020-08-30 12:03:29 +0300

    exchange21

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './UserReducer';
-import {notification} from './notifactionReducer';
+import notification from './notifactionReducer';
 export default combineReducers({
     user,
     notification
-    
+
 });
